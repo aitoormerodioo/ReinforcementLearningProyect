7c3558dd53156d41aa1a3ca6e0c23d88827cc4e6
aitoormerodioo <aitor.merodio@opendeusto.es>
2024-11-27 09:01:51 +0100

    base_structure

---
+++ trial.py
@@ -0,0 +1,14 @@
+import gymnasium
+import numpy as np
+
+# Configuration of the enviroment
+env = gymnasium.make('Ant-v5', xml_file='./mujoco_menagerie/unitree_go1/scene.xml')
+
+#Info about the Env
+observation, info = env.reset()
+print("Espacio de observaciones:", env.observation_space)
+print("Espacio de acciones:", env.action_space)
+
+## TRAINING ###
+
+env.close
