05af629db4225bd21899a27898ccaedbc62fdc79
jonlaasa <jon.lasa@opendeusto.es>
2024-11-27 09:13:24 +0100

    render_mode

--- trial.py
+++ trial.py
@@ -2,7 +2,7 @@ import gymnasium
 import numpy as np
 
 # Configuration of the enviroment
-env = gymnasium.make('Ant-v5', xml_file='./mujoco_menagerie/unitree_go1/scene.xml')
+env = gymnasium.make('Ant-v5', xml_file='./mujoco_menagerie/unitree_go1/scene.xml',render_mode='human')
 
 #Info about the Env
 observation, info = env.reset()
